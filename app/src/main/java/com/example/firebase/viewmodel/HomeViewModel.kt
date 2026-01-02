60641dee146c24f413586fc85b464f28b29d3f6d
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-01-02 20:02:58 +0700

    menambahkan interface statusUiSiswa

---
+++ app/src/main/java/com/example/firebase/viewmodel/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.firebase.viewmodel
+
+import com.example.firebase.modeldata.Siswa
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
