4864c638a4b603c0a8d64d7388f305e86b708c66
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-01-02 20:04:23 +0700

    menambahkan class homeviewmodel

--- app/src/main/java/com/example/firebase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/HomeViewModel.kt
@@ -1,9 +1,21 @@
 package com.example.firebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.firebase.modeldata.Siswa
+import com.example.firebase.repositori.RepositorySiswa
 
 sealed interface StatusUiSiswa {
     data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
+}
+class HomeViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+    init {
+        loadSiswa()
+    }
 }
