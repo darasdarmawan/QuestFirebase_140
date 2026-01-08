b747cba738a3fdeea0cbcb60391661c232122e20
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-01-08 19:52:52 +0700

    tambah fungsi detail siswa screen

---
+++ app/src/main/java/com/example/firebase/view/HalamanDetail.kt
@@ -0,0 +1,19 @@
+package com.example.firebase.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebase.viewmodel.DetailViewModel
+import com.example.firebase.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
