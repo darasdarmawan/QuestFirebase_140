d96e6030e3d8c18a5fda9ce7bd35f4a102eda635
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-01-08 19:43:26 +0700

    tambah ViewModel Factory untuk Detail, dan EditViewModel

--- app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.firebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -12,6 +13,10 @@ fun CreationExtras.aplikasiDataSiswa() : AplikasiDatasSiswa = (
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
-            initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer { DetailViewModel(this.createSavedStateHandle(),aplikasiDataSiswa
+            ().container.repositorySiswa) }
+        initializer { EditViewModel(this.createSavedStateHandle(),aplikasiDataSiswa
+            ().container.repositorySiswa) }
     }
 }
