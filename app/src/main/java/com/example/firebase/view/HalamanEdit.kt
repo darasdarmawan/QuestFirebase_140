3bd2035baad2594e27c6eced58f9e60ff5a5dc43
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-01-08 20:51:49 +0700

    tambah fungsi edit siswa screen

--- app/src/main/java/com/example/firebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/firebase/view/HalamanEdit.kt
@@ -1,2 +1,19 @@
 package com.example.firebase.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebase.viewmodel.EditViewModel
+import com.example.firebase.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
