9fd49c85fc52e60427e438f3dc4db20827c70a07
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-01-08 19:31:59 +0700

    buat DetailViewModel dengan idSiswa dan statusUIDetail

--- app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
@@ -1,9 +1,25 @@
 package com.example.firebase.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.firebase.modeldata.Siswa
+import com.example.firebase.repositori.RepositorySiswa
+import com.example.firebase.view.route.DestinasiDetail
 
 sealed interface StatusUIDetail {
     data class Success(val satuSiswa: Siswa?) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
+class DetailViewModel(savedStateHandle: SavedStateHandle,private val  repositorySiswa: RepositorySiswa):
+    ViewModel() {
+        private val idSiswa: Long =
+            savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+                ?: error("idSIswa tidak ditemukan di SavedStateHandle")
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+    init {
+        getSatuSiswa()
+    }
+    }
