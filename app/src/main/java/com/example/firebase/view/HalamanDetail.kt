93a79f398876abf3d78a4532ee936337edd910ed
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-01-08 20:05:05 +0700

    set modifier untuk detail screen

--- app/src/main/java/com/example/firebase/view/HalamanDetail.kt
+++ app/src/main/java/com/example/firebase/view/HalamanDetail.kt
@@ -1,14 +1,26 @@
 package com.example.firebase.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.firebase.view.route.DestinasiDetail
 import com.example.firebase.viewmodel.DetailViewModel
 import com.example.firebase.viewmodel.PenyediaViewModel
+import com.example.firebase.viewmodel.StatusUIDetail
+import com.example.firebase.viewmodel.StatusUiSiswa
+import com.example.firebase.R
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,6 +38,21 @@ fun DetailSiswaScreen(
                 navigateUp = navigateBack
             )
         },
-
+        floatingActionButton = {
+            val uiState = viewModel.statusUIDetail
+            FloatingActionButton(
+                onClick = {
+                    when(uiState){ is StatusUIDetail.Success->
+                    navigateToEditItem(uiState.satuSiswa!!.id.toInt()) else{}}
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update),
+                )
+            }
+        }, modifier = modifier
     )
 }
